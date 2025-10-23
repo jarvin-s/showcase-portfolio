451483bb01d2b4508e211c3906cec793a0c02d08
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-10-23 10:02:38 +0200

    feat: add animations to hero & navbar

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -12,6 +12,29 @@ const inter = Inter_Tight({
 })
 
 export default function Navbar() {
+    const navbarRef = useRef(null)
+    const homeRef = useRef(null)
+    const workRef = useRef(null)
+    const contactRef = useRef(null)
+
+    useEffect(() => {
+        const tl = gsap.timeline()
+        tl.fromTo(
+            [homeRef.current, workRef.current, contactRef.current],
+            {
+                opacity: 0,
+                y: 50,
+            },
+            {
+                opacity: 1,
+                y: 0,
+                duration: 2,
+                ease: 'power2.out',
+                delay: 0.4,
+                stagger: 0.2,
+            }
+        )
+    }, [])
     const [isMenuOpen, setIsMenuOpen] = useState(false)
     const [hoveredLink, setHoveredLink] = useState<string | null>(null)
     const menuRef = useRef(null)
@@ -48,7 +71,10 @@ export default function Navbar() {
     }, [isMenuOpen])
 
     return (
-        <nav className={`fixed top-0 right-0 z-50 p-6 ${inter.className}`}>
+        <nav
+            ref={navbarRef}
+            className={`fixed top-0 right-0 z-50 p-6 ${inter.className}`}
+        >
             <div className='flex items-center justify-between'>
                 <button
                     onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -59,11 +85,11 @@ export default function Navbar() {
                 </button>
 
                 <div className='hidden md:block'>
-                    <ul className='flex flex-row gap-6'>
-                        <li>
+                    <ul className='flex flex-row items-center gap-6'>
+                        <li ref={homeRef}>
                             <Link
                                 href='/'
-                                className={`text-primary text-3xl font-bold transition-opacity ${
+                                className={`text-primary text-2xl font-bold transition-opacity ${
                                     hoveredLink && hoveredLink !== 'home'
                                         ? 'opacity-30'
                                         : ''
@@ -74,10 +100,10 @@ export default function Navbar() {
                                 Home
                             </Link>
                         </li>
-                        <li>
+                        <li ref={workRef}>
                             <Link
                                 href='/work'
-                                className={`text-primary text-3xl font-bold transition-opacity ${
+                                className={`text-primary text-2xl font-bold transition-opacity ${
                                     hoveredLink && hoveredLink !== 'work'
                                         ? 'opacity-30'
                                         : ''
@@ -87,6 +113,38 @@ export default function Navbar() {
                             >
                                 Work
                             </Link>
+                        </li>
+                        <li
+                            ref={contactRef}
+                            className='flex items-center'
+                            onMouseEnter={() => setHoveredLink('contact')}
+                            onMouseLeave={() => setHoveredLink(null)}
+                        >
+                            <Link
+                                href='/contact'
+                                className={`border-primary rounded-full border border-dotted px-4 py-2 text-2xl font-bold transition-all ${
+                                    hoveredLink && hoveredLink !== 'contact'
+                                        ? 'text-primary/30 border-primary/30'
+                                        : hoveredLink === 'contact'
+                                          ? 'bg-primary text-[#0f0f0f]'
+                                          : 'text-primary'
+                                }`}
+                            >
+                                Contact
+                            </Link>
+                            <div
+                                className={`border-primary cursor-pointer rounded-full border border-dotted p-2 transition-all ${
+                                    hoveredLink && hoveredLink !== 'contact'
+                                        ? 'opacity-30'
+                                        : hoveredLink === 'contact'
+                                          ? 'bg-primary text-[#0f0f0f]'
+                                          : ''
+                                }`}
+                            >
+                                <ArrowRight
+                                    isHovered={hoveredLink === 'contact'}
+                                />
+                            </div>
                         </li>
                     </ul>
                 </div>
@@ -123,6 +181,21 @@ export default function Navbar() {
                             onMouseLeave={() => setHoveredLink(null)}
                         >
                             Work
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href='/contact'
+                            className={`transition-opacity ${
+                                hoveredLink && hoveredLink !== 'contact'
+                                    ? 'opacity-30'
+                                    : ''
+                            }`}
+                            onClick={() => setIsMenuOpen(false)}
+                            onMouseEnter={() => setHoveredLink('contact')}
+                            onMouseLeave={() => setHoveredLink(null)}
+                        >
+                            Contact
                         </Link>
                     </li>
                 </ul>
@@ -162,6 +235,26 @@ export function CloseMenu() {
                 strokeWidth='2'
                 strokeLinecap='round'
                 strokeLinejoin='round'
+            />
+        </svg>
+    )
+}
+
+export function ArrowRight({ isHovered }: { isHovered: boolean }) {
+    return (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='32'
+            height='32'
+            viewBox='0 0 24 24'
+        >
+            <path
+                fill='none'
+                stroke={isHovered ? '#0f0f0f' : '#FFEDCF'}
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='1.5'
+                d='M6 18L18 6m0 0H9m9 0v9'
             />
         </svg>
     )
--- src/components/index/hero.tsx
+++ src/components/index/hero.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import { Anton } from 'next/font/google'
+import gsap from 'gsap'
+import { useEffect, useRef } from 'react'
 
 const anton = Anton({
     subsets: ['latin'],
@@ -7,13 +11,68 @@ const anton = Anton({
 })
 
 export default function Hero() {
+    const firstNameRef = useRef(null)
+    const lastNameRef = useRef(null)
+    const animationRef = useRef<gsap.Context | null>(null)
+
+    useEffect(() => {
+        if (animationRef.current) {
+            animationRef.current.kill()
+        }
+
+        animationRef.current = gsap.context(() => {
+            const tl = gsap.timeline({
+                onComplete: () => {
+                    animationRef.current = null
+                },
+            })
+
+            tl.fromTo(
+                firstNameRef.current,
+                {
+                    opacity: 0,
+                    x: -200,
+                },
+                {
+                    opacity: 1,
+                    x: 0,
+                    duration: 2.5,
+                    ease: 'power4.out',
+                    delay: 0.4,
+                }
+            )
+
+            tl.fromTo(
+                lastNameRef.current,
+                {
+                    opacity: 0,
+                    x: 200,
+                },
+                {
+                    opacity: 1,
+                    x: 0,
+                    duration: 1.5,
+                    ease: 'power4.out',
+                },
+                '<'
+            )
+        })
+
+        return () => {
+            if (animationRef.current) {
+                animationRef.current.kill()
+            }
+        }
+    }, [])
+
     return (
         <div className='relative h-screen'>
             <div className='z-10 flex h-full w-full flex-col items-center justify-center'>
                 <div
                     className={`text-primary relative text-[20vw] font-bold ${anton.className} flex flex-col items-center justify-center text-center leading-[0.88] uppercase`}
                 >
-                    Jarvin Siegers
+                    <span ref={firstNameRef}>Jarvin</span>
+                    <span ref={lastNameRef}>Siegers</span>
                 </div>
             </div>
         </div>
